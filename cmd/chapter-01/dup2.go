189b780f25bc2ec86aa67fe8ee907506e639cb1c
Cory Kim <git@cory.kim>
2021-08-02 14:19:34 -0500

    Add the first few problems

---
+++ cmd/chapter-01/dup2.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		// read from stdin
+		countLines(os.Stdin, counts)
+	} else {
+		for _, filename := range files {
+			f, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%4d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(file *os.File, counts map[string]int) {
+	input := bufio.NewScanner(file)
+	// NOTE: ignoring potential errors from input.Err()
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
---
+++ cmd/chapter-01/echo4.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	const offset = 1
+	for i, arg := range(os.Args[offset:]) {
+		fmt.Println(offset + i, arg)
+	}
+}
